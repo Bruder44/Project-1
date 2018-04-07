b28c4a0fad7460f5308ff75e1fcdaa7eb87a2ade
Matt Bruder <mbruder@students.kennesaw.edu>
2018-04-07 02:40:09 -0400

    updated js, css, html

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,12 +1,41 @@
+$(document).ready(function() {
 var config = {
-    apiKey: "AIzaSyD7QxcGxRT5b7HqxTzllDDK_bMt3LWVqCQ",
-    authDomain: "ecofinder-1522697591597.firebaseapp.com",
-    databaseURL: "https://ecofinder-1522697591597.firebaseio.com",
-    projectId: "ecofinder-1522697591597",
-    storageBucket: "ecofinder-1522697591597.appspot.com",
+     apiKey: "AIzaSyD7QxcGxRT5b7HqxTzllDDK_bMt3LWVqCQ",
+     authDomain: "ecofinder-1522697591597.firebaseapp.com",
+     databaseURL: "https://ecofinder-1522697591597.firebaseio.com",
+     projectId: "ecofinder-1522697591597",
+     storageBucket: "ecofinder-1522697591597.appspot.com",
     messagingSenderId: "728861998432"
-  };
-  firebase.initializeApp(config);
+   };
+   
+   firebase.initializeApp(config);
+  
+  
+ 
+  $("#monthly").on("click", function(event) {
+    event.preventDefault();
+    
+    var database = firebase.database();
+   
+    var userinput = $("#email").val().trim(); 
 
-  var ref = firebase.database().ref();
-  var userinput = firbase.database().ref("pac-input");
+    var newuser = {email:userinput
+    }; 
+    
+    database.ref().push(newuser);
+    
+    console.log(newuser);
+
+    alert("email added!");
+
+    $("#email").val(" ");
+
+    var queryURL = "https://newsapi.org/v2/top-headlines?q=recycling&apiKey=93310ac992ea4b76a7721436c947190e";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) 
+    {console.log(response);
+      $("#links").text(response)
+  })})});
